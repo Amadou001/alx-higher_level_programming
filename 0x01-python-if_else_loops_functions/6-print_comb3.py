c8a4ac2ba535222a242b030a3c86d0e9632f0559
Amadou001 <bamadou634@gmail.com>
2024-01-16 17:57:28 +0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
 for i in range(10):
     for j in range(i+1, 10):
-        print("{}{}, ".format(i, j), end="")
-print()
+        if i < 8:
+            print(f"{i:d}{j:d}", end=", ")
+        else:
+            print(f"{i:d}{j:d}")
+
