613e41b948568212cee83f66749da8e61a990dd8
Amadou001 <bamadou634@gmail.com>
2024-01-17 15:27:36 +0800

    Python - if/else, loops, functions Python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
     if x < 99:
-        print(f"{x:02d}", end=', ')
+        print("{:02d}".format(x), end=', ')
     else:
-        print(f"{x:d}")
-    
+        print("{:d}".format(x))
