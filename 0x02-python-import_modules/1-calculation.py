ce1458e79e1470f34e25c1c17cdfb6fc093a1327
Amadou001 <bamadou634@gmail.com>
2024-01-18 21:24:15 +0800

    0-add.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-#this line ensures that this script is executed when run directly not imported
 if __name__ == "__main__":
-    #import necessary functions from calculator_1 module
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
@@ -10,4 +8,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-    
