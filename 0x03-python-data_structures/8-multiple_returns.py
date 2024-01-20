1bb758cd11127bfbf97cc94e0d4ce643b3682401
Amadou001 <bamadou634@gmail.com>
2024-01-20 23:48:12 +0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     s_length = len(sentence)
     if s_length == 0:
-        return s_length, ('None',)
+        return (s_length, 'None')
     return (s_length, sentence[0])
