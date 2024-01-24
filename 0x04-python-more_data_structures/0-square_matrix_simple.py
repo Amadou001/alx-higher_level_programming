33003f3837f821dac68ce9a95b949b96cc53dd6d
Amadou001 <bamadou634@gmail.com>
2024-01-24 15:45:53 +0800

    Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix =[]
+    for rows in matrix:
+        new_row = []
+        for element in rows:
+            new_row.append(element ** 2)
+        new_matrix.append(new_row)
+
+    return new_matrix
