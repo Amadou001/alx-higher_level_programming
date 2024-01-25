8ba3709096413e066615a45afab0e8e4aea769ac
Amadou001 <bamadou634@gmail.com>
2024-01-25 14:45:10 +0800

    Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    my_set = set()
+    num_sum = 0
+    for number in my_list:
+        my_set.add(number)
+    new_list = list(my_set)
+    print(new_list)
+    for x in new_list:
+        num_sum +=  x
+    return num_sum
