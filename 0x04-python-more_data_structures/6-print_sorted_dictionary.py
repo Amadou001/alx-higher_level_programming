f0356d18c42a085ed44525f0b7d9595b534e2b6b
Amadou001 <bamadou634@gmail.com>
2024-01-25 16:43:24 +0800

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    b_dictionary = dict(sorted(a_dictionary.items()))
-    for element, value in b_dictionary.items():
+    for element, value in sorted(a_dictionary.items()):
         print("{} : {}".format(element, value))
