021082e5db98e4ce73023b650bd9f65494467a43
Amadou001 <bamadou634@gmail.com>
2024-02-07 01:30:47 +0800

    Python - Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    """This is a square class that is
-    empty"""
+    """
+    A class representing a square.
+
+    This class doesn't have any attributes or methods yet.
+    """
     pass
