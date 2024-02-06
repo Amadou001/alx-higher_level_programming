0a4e846c0627b429b26557aa6eefff793a6a7874
Amadou001 <bamadou634@gmail.com>
2024-02-06 14:24:08 +0800

    Python - Exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    element_printed = 0
+    for i in range(x):
+        try:
+              print("{:d}".format(my_list[i]), end="")
+              element_printed += 1
+        except IndexError:
+             pass
+        except (ValueError, TypeError):
+             pass
+    print()
+    return element_printed
