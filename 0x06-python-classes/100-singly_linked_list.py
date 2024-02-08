8b5fac0ba3fef0b51825ef574d391e25b9c37ca4
Amadou001 <bamadou634@gmail.com>
2024-02-08 19:38:22 +0800

    Python - Classes and Objects

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Module that defines a node"""
 
-
 class Node:
     """Class that defines a node"""
 
     def __init__(self, data, next_node=None):
         """Constructor
-
+        
             Args:
                 data (int): data of the node
                 next_node (Node): optional
@@ -29,7 +28,7 @@ class Node:
         """Function that defines the value of the private instance __data"""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        self.__data = value
+        self.__data =  value
 
     @property
     def next_node(self):
@@ -50,22 +49,20 @@ class SinglyLinkedList:
     def __init__(self):
         """Constructor function"""
         self.__head = None
-
+    
     def sorted_insert(self, value):
-        """Inserts a new Node into the correct sorted\
-        position in the list (increasing order)"""
+        """Inserts a new Node into the correct sorted position in the list (increasing order)"""
         new_node = Node(value)
         if self.__head is None or value < self.__head.data:
             new_node.next_node = self.__head
             self.__head = new_node
         else:
             current = self.__head
-            while current.next_node is not None\
-                    and value >= current.next_node.data:
+            while current.next_node is not None and value >= current.next_node.data:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
-
+    
     def __str__(self):
         """Returns a string representation of the linked list."""
         result = ""
