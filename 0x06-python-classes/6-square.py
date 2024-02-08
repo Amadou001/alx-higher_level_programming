47cede1ffa2c6cb9c7fd4cdb1dbf786a82fc49e0
Amadou001 <bamadou634@gmail.com>
2024-02-08 22:30:52 +0800

    Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                if self.__position[1] <= 1:
+                if self.__position[1] <= 0:
                     for k in range(self.__position[0]):
                         print(end=" ")
                     for j in range(self.__size):
