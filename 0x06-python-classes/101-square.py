f2543fbd06d33d790fa8bad7b30a8f4d98ea1a42
Amadou001 <bamadou634@gmail.com>
2024-02-08 23:51:16 +0800

    Python - Classes and Objects

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -3,30 +3,20 @@
 
 
 class Square:
-    """Class that defines a square"""
+    """Defines a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Constructor method
-        
-            Args:
-                size(optional, int): size of the square
-                position(optional tuple): tuple of positive integers
-        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Function that retrieves the private instance attribute size"""
+        """Retrieve the size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Function that sets the value of __size private attribute
-        
-        Args: 
-            value (int): positive integer
-        """
+        """Set the size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -35,55 +25,47 @@ class Square:
 
     @property
     def position(self):
-        """Function that retrieves the value of __position private instance"""
+        """Retrieve the position of the square"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Function that sets the value of __position attribute
-        
-        Args:
-            value: tuple of positive integers
-        """
-        if not isinstance(value, tuple) or len(value) != 2 or\
-            not all(isinstance(x, int) for x in value) or\
-            not all(x >= 0 for x in value):
-            raise TypeError("position must be a tuple of 2 positive integer")
+        """Set the position of the square"""
+        if not isinstance(value, tuple) or len(value) != 2 \
+                or not all(isinstance(num, int) for num in value) \
+                or any(num < 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Function that returns the area of the square"""
+        """Calculate the area of the square"""
         return self.__size ** 2
 
     def my_print(self):
-        """Prints the area by using # and spaces"""
+        """Print the square with the character '#'"""
         if self.__size == 0:
             print()
-        else:
-                if self.__position[1] > 0:
-                    for j in range(self.__size):
-                        print("#" * self.__size)
-                else:
-                    for j in range(self.__size):
-                        print(" " * self.__position[0], "#" * self.__size)
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
 
     def __str__(self):
-         """Prints the area by using # and spaces"""
-         string_square = ""
-         if self.__size == 0:
-             string_square = "\n"
-         else:
-                 if self.__position[1] > 1:
-                     for i in range(self.__size):
-                         string_square += "#" * self.__size + "\n"
-                 else:
-                     for i in range(self.__size):
-                         string_square += " " * self.__position[0] + "#" * self.__size + "\n"
-         return string_square
+        """String representation of the square"""
+        square_str = ""
+        if self.__size == 0:
+            return square_str
 
-                     
-                     
-        
+        square_str += "\n" * self.__position[1]
 
-        
+        for _ in range(self.__size):
+            square_str += " " * self.__position[0] + "#" * self.__size + "\n"
 
+        return square_str
+
+    def __repr__(self):
+        """Representation of the square"""
+        return self.__str__()
