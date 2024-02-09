acded65c23c141ba0ab044ae85ae092ec4cb6599
Amadou001 <bamadou634@gmail.com>
2024-02-09 12:27:05 +0800

    Python - Classes and Objects

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -6,7 +6,7 @@ class Square:
     """Square Class"""
     def __init__(self, size=0):
         """Constructor
-        
+
             Args:
                 size(int , optional): size of the square side
         """
@@ -16,11 +16,11 @@ class Square:
     def size(self):
         """Retrieves the private attribute __size"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """Sets the private attribute __size
-        
+
             Args:
                 value(int): positive integer
         """
@@ -28,3 +28,44 @@ class Square:
             raise TypeError("size must be a number")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Equality"""
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return NotImplemented
+
+    def __ne__(self, other):
+        """Not equal"""
+        if isinstance(other, Square):
+            return self.area() != other.area()
+        return NotImplemented
+
+    def __gt__(self, other):
+        """Greater than"""
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return NotImplemented
+
+    def __ge__(self, other):
+        """Greater than or equal"""
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+        return NotImplemented
+
+    def __lt__(self, other):
+        """less than"""
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return NotImplemented
+
+    def __le__(self, other):
+        """Less than or equal"""
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return NotImplemented
