748bece7fccf8dd4444c39e6a88e424571ee2a6e
Amadou001 <bamadou634@gmail.com>
2024-02-11 16:02:57 +0800

    Python - Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Integer Addition
+
+Use add_integer function to perform the task
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two numbers
+
+        Args:
+            a(int or float):
+            b(int or float): optional
+
+        Returns: the sum in type int
+
+    """
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
