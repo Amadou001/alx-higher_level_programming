3bf71e2fb28ed3207c39b0b82f5820e46a40536d
Amadou001 <bamadou634@gmail.com>
2024-02-12 21:36:22 +0800

    Python - Test-driven development

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module for printing a square
+with the character #
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with #
+
+        Args:
+            size(positive integer): size length of the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for _ in range(size):
+        print("#" * size)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile('tests/4-print_square.txt')
