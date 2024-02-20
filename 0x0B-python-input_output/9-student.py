bc90c536ec4e6730bf245a62c752139fa7f4853c
Amadou001 <bamadou634@gmail.com>
2024-02-21 00:25:20 +0800

    Python - Input/Output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""____MODULE_____"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        # Create a dictionary representation of the Student instance
+        student_dict = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
+        return student_dict
