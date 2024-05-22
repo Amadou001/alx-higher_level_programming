fdc09c420cfdd4bed4c925abdd1d94a183ace8ec
Amadou001 <bamadou634@gmail.com>
2024-05-22 18:58:41 +0800

    Updates

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,12 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- Create the database if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not already exist and set the password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+
+-- Grant SELECT privilege on the database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply the privilege changes
 FLUSH PRIVILEGES;
