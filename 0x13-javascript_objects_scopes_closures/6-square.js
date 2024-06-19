6394904629fbd4d017476038ce957454a5e7cb34
Amadou001 <bamadou634@gmail.com>
2024-06-20 04:41:34 +0800

    JavaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+class Square extends Square1 {
+  charPrint (c) {
+    let line = '';
+    if (c === undefined) {
+      for (let i = 0; i < this.width; i++) {
+        line += 'X';
+      }
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        line += c;
+      }
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+}
+module.exports = Square;
