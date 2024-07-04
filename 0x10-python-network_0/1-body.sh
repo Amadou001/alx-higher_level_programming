d95e51119fcf41f76e72c05437a7009c3d5f1681
Amadou001 <bamadou634@gmail.com>
2024-07-05 01:56:33 +0800

    Python - Network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,15 +2,4 @@
 # sends a GET request to the URL, and displays the body of the response
 # Display only body of a 200 status code response
 
-URL="$1"
-
-# Send a GET request and capture the response headers and body
-response=$(curl -s -w "%{http_code}" "$URL")
-body="${response%[0-9][0-9][0-9]}"
-status_code="${response: -3}"
-
-# Check if the status code is 200
-if [ "$status_code" -eq 200 ]; then
-  # Display the body of the response
-  echo "$body"
-fi
+curl -sL "$1"
